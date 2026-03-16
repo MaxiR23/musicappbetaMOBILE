603da99778d0669f6779395a7d6ed5c8d5c888ea
MaxiRebolo <maximilianorebolo@gmail.com>
2026-03-16 02:41:12 -0300

    OK: important change in config

--- constants/config.ts
+++ constants/config.ts
@@ -2,6 +2,6 @@ export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const STREAM_ENDPOINT = "https://www.youtube.com/youtubei/v1/player?prettyPrint=false";
-export const STREAM_CLIENT_NAME = "ANDROID_VR";
-export const STREAM_CLIENT_VERSION = "1.56.21";
+export const STREAM_ENDPOINT = process.env.EXPO_PUBLIC_STREAM_ENDPOINT;
+export const STREAM_CLIENT_NAME = process.env.EXPO_PUBLIC_STREAM_CLIENT_NAME;
+export const STREAM_CLIENT_VERSION = process.env.EXPO_PUBLIC_STREAM_CLIENT_VERSION;
